f3b7e97f3e4e0dce08ddc10ffc6563398cb407a3
bankingloan <tamcha2809@gmail.com>
2016-05-06 17:09:58 +0700

    add fragments + justified Textview

--- app/src/main/java/chau/bankingloan/PrintFragment.java
+++ app/src/main/java/chau/bankingloan/PrintFragment.java
@@ -1,7 +1,12 @@
 package chau.bankingloan;
 
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,12 +17,15 @@ import android.view.ViewGroup;
 public class PrintFragment extends Fragment
 {
     View rootView;
+    JustifyTextView test;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         setHasOptionsMenu(true);
         rootView = inflater.inflate(R.layout.fragment_print, container, false);
+        test = (JustifyTextView) rootView.findViewById(R.id.test);
+        test.setText("Please wait while we process your application." + "\r\n" + "This may take a few minutes...");
         return rootView;
     }
 }
